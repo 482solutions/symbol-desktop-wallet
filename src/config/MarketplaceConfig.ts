d0e983aca1ef9944df66f44e662e392efbaa1d11
Vlad Boichenko <v.boichenko@4irelabs.com>
2021-03-25 14:53:38 +0200

    feat: Updated to v1.0.0

--- src/config/MarketplaceConfig.ts
+++ src/config/MarketplaceConfig.ts
@@ -1,6 +1,6 @@
 const marketplaceServer = 'http://207.154.219.45:1823/api/v1/';
 const ipfsNode = 'https://ipfs.infura.io:5001';
-const serviceAccountAddress = 'ND6AAPLHXTDVJEU33VTLH65LG5FPKJQBZPLX6KQ';
+const serviceAccountAddress = 'TBQRYGBRTOIIOVQQQENMCTL2RLW2DV3UPQ3RG3I';
 const hoursList: number[] = [6, 12, 24, 48];
 const MarketplaceConfig = { marketplaceServer, hoursList: hoursList, ipfsNode, serviceAccountAddress };
 console.log('Marketplace Config resolved!', MarketplaceConfig);
